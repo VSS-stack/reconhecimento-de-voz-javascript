54d102937b899d4245fec4cf5d8553720c843934
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-01-30 21:56:54 -0300

    dando continuidade a parte de validação

--- app/validacao.js
+++ app/validacao.js
@@ -2,11 +2,24 @@ function verificaValorDoChute(chute) {
     const numero = parseInt(chute);
 
     if(chuteForInvalido(numero)) {
-        console.log('valor invalido');
+        elementoChute.innerHTML +=`<div>Valor inválido</div>`;
+        return;
     }
 
     if(numeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        elementoChute.innerHTML += `<div>Valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}</div>`
+        elementoChute.innerHTML += `<div>Opa! o número precisa estar entre ${menorValor} e ${maiorValor}</div>`
+        return;
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    }else if(numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`;
+    }else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-arrow-up-long"></i></div>`;
     }
 }
 
