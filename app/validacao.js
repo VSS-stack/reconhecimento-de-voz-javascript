bc778511d146f790e1acd5dcc3d006f82806f918
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-01-31 21:29:14 -0300

    adicionando botão jogar novamente

--- app/validacao.js
+++ app/validacao.js
@@ -15,6 +15,8 @@ function verificaValorDoChute(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         ` //nesse if termina o jogo pois ele muda a estrutura do html e quebra o fluxo do script
     }else if(numero > numeroSecreto) {
         elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-arrow-down-long"></i></div>`;
@@ -30,4 +32,12 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
     return numero > maiorValor || numero < menorValor;
 }
+
+document.body.addEventListener('click', e => {
+    if(e.target.id === 'jogar-novamente') {
+        window.location.reload();
+    }
+})
+
+console.log(numeroSecreto);
 
